b3a9fdeaef52073f5ffececd0db8e7d4e07787ab
gabcytn <cayetanogabriel03@gmail.com>
2024-11-01 22:47:44 +0800

    Create new entity for tech stack

--- src/main/java/com/gabcytn/springjparest/Model/TechStack.java
+++ src/main/java/com/gabcytn/springjparest/Model/TechStack.java
@@ -1,4 +1,19 @@
 package com.gabcytn.springjparest.Model;
 
+import jakarta.persistence.*;
+@Entity
 public class TechStack {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String technology;
+
+    @ManyToOne
+    private Job job;
+
+    public String getTechnology() {
+        return technology;
+    }
+
 }
