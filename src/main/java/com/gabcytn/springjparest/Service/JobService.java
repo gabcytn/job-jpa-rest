d892dee93e219ac592c9f2c80add8c0972cad8ea
gabcytn <cayetanogabriel03@gmail.com>
2024-11-01 22:48:24 +0800

    Call jpa repo findAll method

--- src/main/java/com/gabcytn/springjparest/Service/JobService.java
+++ src/main/java/com/gabcytn/springjparest/Service/JobService.java
@@ -1,4 +1,23 @@
 package com.gabcytn.springjparest.Service;
 
+import com.gabcytn.springjparest.Model.Job;
+import com.gabcytn.springjparest.Model.TechStack;
+import com.gabcytn.springjparest.Repository.JobRepository;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Lazy
 public class JobService {
+    private final JobRepository jobRepository;
+
+    public JobService(JobRepository jobRepository) {
+        this.jobRepository = jobRepository;
+    }
+
+    public List<Job> getAllJobs() {
+        return jobRepository.findAll();
+    }
 }
