d9276c45b4f1d85e931a841c251f1b2edfcb4446
gabcytn <cayetanogabriel03@gmail.com>
2024-11-01 22:46:07 +0800

    Create GET mapping for /jobs

--- src/main/java/com/gabcytn/springjparest/Controller/JobController.java
+++ src/main/java/com/gabcytn/springjparest/Controller/JobController.java
@@ -1,4 +1,25 @@
 package com.gabcytn.springjparest.Controller;
 
+import com.gabcytn.springjparest.Model.Job;
+import com.gabcytn.springjparest.Service.JobService;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@Lazy
 public class JobController {
+
+    private final JobService jobService;
+
+    public JobController(JobService jobService) {
+        this.jobService = jobService;
+    }
+
+    @GetMapping("/jobs")
+    public List<Job> getJobs() {
+        return jobService.getAllJobs();
+    }
 }
