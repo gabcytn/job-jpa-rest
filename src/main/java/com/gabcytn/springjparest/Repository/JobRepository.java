5b679c53debe8f6c67a2a5cecb02585a9f04812a
gabcytn <cayetanogabriel03@gmail.com>
2024-11-01 22:48:04 +0800

    Extend jpa repository

--- src/main/java/com/gabcytn/springjparest/Repository/JobRepository.java
+++ src/main/java/com/gabcytn/springjparest/Repository/JobRepository.java
@@ -1,4 +1,9 @@
 package com.gabcytn.springjparest.Repository;
 
-public interface JobRepository {
+import com.gabcytn.springjparest.Model.Job;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JobRepository extends JpaRepository<Job, Integer> {
 }
