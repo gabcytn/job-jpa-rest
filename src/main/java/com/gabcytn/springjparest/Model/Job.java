fa3a3fc15368fd0749e983c36e228891cb979307
gabcytn <cayetanogabriel03@gmail.com>
2024-11-01 21:03:11 +0800

    Create job model

---
+++ src/main/java/com/gabcytn/springjparest/Model/Job.java
@@ -0,0 +1,51 @@
+package com.gabcytn.springjparest.Model;
+
+import java.util.List;
+
+public class Job {
+    private int id;
+    private String title;
+    private String description;
+    private int experience;
+    private List<String> techStack;
+
+    public List<String> getTechStack() {
+        return techStack;
+    }
+
+    public void setTechStack(List<String> techStack) {
+        this.techStack = techStack;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+}
