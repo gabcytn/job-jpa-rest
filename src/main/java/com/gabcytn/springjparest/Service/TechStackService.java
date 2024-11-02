11d5b5107f01a31446485d4c92be2175104c481a
gabcytn <cayetanogabriel03@gmail.com>
2024-11-02 11:37:46 +0800

    Create tech stack interface

--- src/main/java/com/gabcytn/springjparest/Service/TechStackService.java
+++ src/main/java/com/gabcytn/springjparest/Service/TechStackService.java
@@ -1,4 +1,23 @@
 package com.gabcytn.springjparest.Service;
 
+import com.gabcytn.springjparest.Model.TechStack;
+import com.gabcytn.springjparest.Repository.TechStackRepo;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class TechStackService {
+
+    private final TechStackRepo techStackRepo;
+
+    public TechStackService(TechStackRepo techStackRepo) {
+        this.techStackRepo = techStackRepo;
+    }
+
+    public void addTechStack(List<String> techStack, int id) {
+        for(String tech: techStack) {
+            techStackRepo.save(new TechStack(id, tech));
+        }
+    }
 }
