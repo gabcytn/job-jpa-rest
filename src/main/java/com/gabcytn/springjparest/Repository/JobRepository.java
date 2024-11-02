09fda747683a28c102c92c746a92356151fe7b7a
gabcytn <cayetanogabriel03@gmail.com>
2024-11-02 11:36:49 +0800

    Add order by id asc method

--- src/main/java/com/gabcytn/springjparest/Repository/JobRepository.java
+++ src/main/java/com/gabcytn/springjparest/Repository/JobRepository.java
@@ -4,6 +4,9 @@ import com.gabcytn.springjparest.Model.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface JobRepository extends JpaRepository<Job, Integer> {
+    List<Job> findAllByOrderByIdAsc();
 }
